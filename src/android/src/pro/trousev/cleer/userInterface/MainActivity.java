0d745ae7fdc3076ae939e5b1f07fb15b763b278d
Bazhenov Sergey <bazhenov1994@gmail.com>
2013-04-19 18:59:14 +0400

    Lib added to .classpath

--- src/android/src/pro/trousev/cleer/userInterface/MainActivity.java
+++ src/android/src/pro/trousev/cleer/userInterface/MainActivity.java
@@ -4,8 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import pro.trousev.cleer.userInterface.R;
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
