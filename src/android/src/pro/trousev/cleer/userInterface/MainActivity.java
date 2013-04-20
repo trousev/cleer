ee6b596797c4c7bc151cc32185150a0ed4d342a3
Bazhenov Sergey <bazhenov1994@gmail.com>
2013-04-20 15:31:02 +0400

    Added Core Items storage

--- src/android/src/pro/trousev/cleer/userInterface/MainActivity.java
+++ src/android/src/pro/trousev/cleer/userInterface/MainActivity.java
@@ -3,15 +3,25 @@ package pro.trousev.cleer.userInterface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import pro.trousev.cleer.CoreItems;
+import pro.trousev.cleer.Plugin;
+import pro.trousev.cleer.Plugin.Interface;
+import pro.trousev.cleer.Console;
+import pro.trousev.cleer.ConsoleOutput;
+import pro.trousev.cleer.Database;
+import pro.trousev.cleer.Library;
+import pro.trousev.cleer.Player;
+import pro.trousev.cleer.Queue;
 import android.os.Bundle;
 import android.view.View;
 import pro.trousev.cleer.userInterface.R;
 
-public class MainActivity extends FragmentActivity{
+public class MainActivity extends FragmentActivity {
 	private FragmentTransaction fTrans;
 	private Fragment playBar, mainMenu;
+
 	/** Called when the activity is first created. */
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -24,10 +34,46 @@ public class MainActivity extends FragmentActivity{
         fTrans.add(R.id.play_bar, playBar).commit();
         fTrans=getSupportFragmentManager().beginTransaction();
         fTrans.add(R.id.work_space, mainMenu).commit();
+        // TODO set implementations here
+        final Database db = null;
+		final Library lib = null;
+	    final Player player = null;
+	    final Queue queue = null;
+	    CoreItems.setCoreItems(new Plugin.Interface() {
+			
+			@Override
+			public Database storage() {
+				return db;
+			}
+			
+			@Override
+			public Library library() {
+				return lib;
+			}
+
+			@Override
+			public Console console() {
+				return null;
+			}
+
+			@Override
+			public Player player() {
+				return player;
+			}
+
+			@Override
+			public Queue queue() {
+				return queue;
+			}
+
+			@Override
+			public ConsoleOutput output(){
+				return null;
+			}
+	    });
     }
-    
-    public void onExit(View sender)
-    {
-    	System.exit(0);
-    }
+
+	public void onExit(View sender) {
+		System.exit(0);
+	}
 }
