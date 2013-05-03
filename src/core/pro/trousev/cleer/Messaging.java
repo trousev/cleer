dc2b4ce641a4a267bf9120aea5ef20a99ea9a1ce
Vladimir <volod_kuzn@macbook.local>
2013-05-03 20:32:45 +0400

    Multithread messaging implemented

--- src/core/pro/trousev/cleer/Messaging.java
+++ src/core/pro/trousev/cleer/Messaging.java
@@ -42,33 +42,52 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.lang.Thread;
 
 public class Messaging {
-	public interface Message
-	{
-		
+	public interface Message {
+
 	}
-	public interface Event
-	{
+
+	public interface Event {
 		void messageReceived(Message message);
 	}
+
+	private static class RespondTask implements Runnable {
+
+		private final Event e;
+
+		private final Message m;
+
+		RespondTask(Event e, Message m) {
+			this.e = e;
+			this.m = m;
+		}
+
+		@Override
+		public void run() {
+			e.messageReceived(m);
+		}
+	}
+
 	@SuppressWarnings("rawtypes")
-	private static Map<Class, List<Event> > _responders = new ConcurrentHashMap<Class, List<Event>>();
+	private static Map<Class, List<Event>> _responders = new ConcurrentHashMap<Class, List<Event>>();
+
 	@SuppressWarnings("rawtypes")
-	public static void subscribe(Class type, Event event)
-	{
+	public static void subscribe(Class type, Event event) {
 		List<Event> events = _responders.get(type);
-		if(events == null)
-		{
+		if (events == null) {
 			events = new ArrayList<Messaging.Event>();
 			_responders.put(type, events);
 		}
 		events.add(event);
 	}
-	public static void fire(Message message)
-	{
+
+	public static void fire(Message message) {
 		List<Event> events = _responders.get(message.getClass());
-		if(events != null) for(Event e: events)
-			e.messageReceived(message);
+		if (events != null)
+			for (Event e : events) {
+				new Thread(new RespondTask(e, message));
+			}
 	}
 }
