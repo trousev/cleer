210a2ac8dafb55a1ec201eefa4f47df697b80aa6
Vladimir <volod_kuzn@macbook.local>
2013-05-08 00:54:01 +0400

    Appropriate exception added

--- src/android/src/pro/trousev/cleer/android/service/MediaScanner.java
+++ src/android/src/pro/trousev/cleer/android/service/MediaScanner.java
@@ -1,15 +1,7 @@
 package pro.trousev.cleer.android.service;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-
-import org.jaudiotagger.audio.exceptions.CannotReadException;
-import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
-import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
-import org.jaudiotagger.tag.TagException;
-
 import pro.trousev.cleer.Item;
 import pro.trousev.cleer.sys.TrackImpl;
 import android.content.Context;
@@ -19,22 +11,40 @@ import android.provider.MediaStore;
 
 public class MediaScanner {
 	private Context context;
-	public 
-	MediaScanner(Context cont){
+
+	public MediaScanner(Context cont) {
 		context = cont;
 	}
-	public ArrayList<Item> scanner() throws CannotReadException, IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException {
+
+	public static class MediaScannerException extends Exception {
+		private static final long serialVersionUID = -1028575879236687565L;
+
+		MediaScannerException(String reason) {
+			super(String.format("MediaScannerException: ", reason));
+		}
+
+	}
+
+	public ArrayList<Item> scanner() throws MediaScannerException {
 		String[] proj = { MediaStore.Audio.Media.DATA };
 		Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
 		ArrayList<Item> data = new ArrayList<Item>();
-		Cursor cursor = context.getContentResolver().query(uri, proj,null, null, null);
+		Cursor cursor = context.getContentResolver().query(uri, proj, null,
+				null, null);
 		if (cursor.getCount() > 0) {
-			for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-				int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+			for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor
+					.moveToNext()) {
+				int columnIndex = cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
 				Uri filePathUri = Uri.parse(cursor.getString(columnIndex));
 				String filename = filePathUri.getPath().toString();
-				File file=new File(filename);
-				TrackImpl track=new TrackImpl(file);
+				File file = new File(filename);
+				TrackImpl track = null;
+				try { // FIXME Make one exception for TrackImpl
+					track = new TrackImpl(file);
+				} catch (Exception e) {
+					throw new MediaScannerException("Cannot create TrackImpl");
+				}
 				data.add(track);
 			}
 		}
