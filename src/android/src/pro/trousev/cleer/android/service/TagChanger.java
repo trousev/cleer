529eab8a2bff3799d3be434feddcdc5b3665c503
Orazbayev Akbar <akbarsky93@mail.ru>
2013-05-13 02:09:29 +0400

    scanFIX

---
+++ src/android/src/pro/trousev/cleer/android/service/TagChanger.java
@@ -0,0 +1,11 @@
+package pro.trousev.cleer.android.service;
+
+import pro.trousev.cleer.sys.TrackImpl;
+import java.io.*;
+
+public class TagChanger {
+	public TrackImpl TagChenger(TrackImpl track){
+		//String a=new String(track.tag("title").getBytes("windows-1251"),"utf-8");
+		return track;
+	};
+}
