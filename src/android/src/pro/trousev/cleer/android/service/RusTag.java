4cd617a4e6482a8c0f2f6d191913a41c7ca7f737
Orazbayev Akbar <akbarsky93@mail.ru>
2013-05-21 15:30:14 +0400

    Rustag!

--- src/android/src/pro/trousev/cleer/android/service/RusTag.java
+++ src/android/src/pro/trousev/cleer/android/service/RusTag.java
@@ -15,16 +15,9 @@ public class RusTag {
 		int i,x=0;
 		try{
 			for(i=0;i<s.length();i++){
-<<<<<<< HEAD
 				if (((int)s.charAt(i)>192)&&((int)s.charAt(i)<255)){x=x++;}}
 			if (x>1){return s;};
 				return C.decode(buffer.wrap(s.getBytes("UTF-16LE"))).toString();
-=======
-				if ((int)s.charAt(i)<0400){x=x++;}}
-			if (x>0){
-				return C.decode(buffer.wrap(s.getBytes("UTF-16LE"))).toString();}
-				else return s;
->>>>>>> 12379aede7641254747b108f56d9951389156b75
 		}catch(UnsupportedEncodingException e){
 			return s;
 		}
