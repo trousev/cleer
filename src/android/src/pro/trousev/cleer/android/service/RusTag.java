786d8110d2d71834b526509e380e29d111434e6f
Orazbayev Akbar <akbarsky93@mail.ru>
2013-05-21 15:12:09 +0400

    QF1

--- src/android/src/pro/trousev/cleer/android/service/RusTag.java
+++ src/android/src/pro/trousev/cleer/android/service/RusTag.java
@@ -12,10 +12,12 @@ public class RusTag {
 			return s;
 		Charset C= Charset.forName("windows-1251");
 		ByteBuffer buffer=null;
+		int i,x=0;
 		try{
-				if ((int)s.charAt(0)<0400){
-				return C.decode(buffer.wrap(s.getBytes("UTF-16LE"))).toString();}
-				else return s;
+			for(i=0;i<s.length();i++){
+				if (((int)s.charAt(i)>192)&&((int)s.charAt(i)<255)){x=x++;}}
+			if (x>1){return s;};
+				return C.decode(buffer.wrap(s.getBytes("UTF-16LE"))).toString();
 		}catch(UnsupportedEncodingException e){
 			return s;
 		}
